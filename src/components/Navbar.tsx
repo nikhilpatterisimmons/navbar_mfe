52d688038097834f3661f1cb96f976186b556352
Nikhil Patteri <nikhilpatteri@gmail.com>
2022-03-28 12:45:05 +0530

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { NavLink, BrowserRouter } from "react-router-dom";
 import { LogoWrapper, NavigationBar, NavItem } from "./components.styled";
 import MriSimmonsLogo from "./images/MriSimmonsLogo";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyleNavLink = styled(NavLink)`
   color: white;
@@ -12,17 +12,20 @@ const Navbar = () => {
   // const navigate = useNavigate();
   return (
     <BrowserRouter>
-    <NavigationBar>
-      <LogoWrapper>
-        <MriSimmonsLogo />
-      </LogoWrapper>
-      <NavItem>
-        <StyleNavLink to="crosstab">Crosstab</StyleNavLink>
-      </NavItem>
-      <NavItem>
-        <StyleNavLink to="catalyst">Catalyst</StyleNavLink>
+      <NavigationBar>
+        <LogoWrapper>
+          <MriSimmonsLogo />
+        </LogoWrapper>
+        <NavItem>
+          <StyleNavLink to="crosstab">Crosstab</StyleNavLink>
         </NavItem>
-    </NavigationBar>
+        <NavItem>
+          <StyleNavLink to="catalyst">Catalyst</StyleNavLink>
+        </NavItem>
+        <NavItem>
+          <StyleNavLink to="datahub">Datahub</StyleNavLink>
+        </NavItem>
+      </NavigationBar>
     </BrowserRouter>
   );
 };
